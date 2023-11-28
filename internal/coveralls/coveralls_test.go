5a01523358ee739885ce83a17aaa1999bf6a3f98
Adam Reed <adam.reed@synd.io>
2023-11-27 20:09:42 -0700

    add coverage

--- internal/coveralls/coveralls_test.go
+++ internal/coveralls/coveralls_test.go
@@ -54,3 +54,28 @@ func TestOdd(t *testing.T) {
 		})
 	}
 }
+
+func TestIsTwo(t *testing.T) {
+	cases := []struct {
+		name  string
+		input int64
+		pass  bool
+	}{
+		{
+			"two",
+			2,
+			true,
+		},
+		{
+			"not two",
+			3,
+			false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			assert.Equal(t, c.pass, IsTwo(c.input))
+		})
+	}
+}
